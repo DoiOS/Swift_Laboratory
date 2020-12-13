103f1aa76a2b6c1929508703a2dbddd514918a1c
Kim Minseop <44546283+miseop25@users.noreply.github.com>
2020-12-13 18:58:03 +0900

    [Add] 백준 2947 나무조각 (#4)
    - 버블정렬 구현문제

---
+++ AlgorithmMaster/Backjoon/back_joon_2947/back_joon_2947.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+func bubbleSort(arr: Array<Int>) -> Array<Int> {
+    var temp = arr
+    let len = arr.count
+    var buf = 0
+    for i in stride(from: len-1, to: -1, by: -1) {
+        for j in stride(from: 0, to: i, by: 1) {
+            if temp[j] > temp[j+1] {
+                buf = temp[j]
+                temp[j] = temp[j+1]
+                temp[j+1] = buf
+                let p = temp.map({(value) in String(value) })
+                print(p.joined(separator: " "))
+            }
+        }
+    }
+    return temp
+}
+
+var arr =  readLine()!.components(separatedBy: " ").map({ (value) in Int(value)! })
+let answer = bubbleSort(arr: arr)
+
+
